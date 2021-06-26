2b4b1c044d347567d45ec101452546573098145a
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-06-26 19:44:44 +0900

    extract hospital title name

--- src/public/classes/DomDocumentParser.php
+++ src/public/classes/DomDocumentParser.php
@@ -3,6 +3,7 @@ class DomDocumentParser
 {
 	private $doc;
 	private $xpath;
+	public $results = array();
 
 	public function __construct($url)
 	{
@@ -26,22 +27,39 @@ class DomDocumentParser
 	 * @return array
 	 */
 
+	// $results = 
+	// [
+	// 	'〇〇病院' =>[
+	// 		'総合点' => 1,
+	// 		'〇〇' =>2,
+	// 		'〇〇' =>1,
+	// 	],
+	// 	'△△病院' =>[
+	// 		'総合点' => 1,
+	// 		'〇〇' =>2,
+	// 		'〇〇' =>1,
+	// 	]
+	// 	];
+
 	public function loopNode()
 	{
-		$result_array = array();
 
+		$hospitalName = $this->getHospitalName();
+		$rateArray = array();
 
 		for ($i = 1; $i < 8; $i++) {
 
 			if ($i === 1) {
-				$result_array[$i - 1]['title'] = '総合点';
-				$result_array[$i - 1]['rate'] = (int)$this->getTotalScore();
+				$rateArray['総合点'] = (int)$this->getTotalScore();
 				continue;
 			}
-			$result_array[$i - 1]['title'] = $this->getTitle($i);
-			$result_array[$i - 1]['rate'] = $this->outputRateNum($i);
+
+			$key = $this->getTitle($i);
+			$value = $this->outputRateNum($i);
+			$rateArray[$key] = $value;
 		}
-		return $result_array;
+		$this->results[$hospitalName] = $rateArray;
+		return $this->results;
 	}
 
 	/**
@@ -80,5 +98,13 @@ class DomDocumentParser
 	private function getTotalScore()
 	{
 		return $this->xpath->query("//div[@class='table__row']//span[@class='score']")->item(0)->nodeValue;
+	}
+	/**
+	 * 病院名
+	 * @return string
+	 */
+	private function getHospitalName()
+	{
+		return $this->xpath->query("//h1[@class='hospital-top__container__title']")->item(0)->nodeValue;
 	}
 }
